7594f67d8ae3066422e918a6280941d93443b2ba
Leticia <leticiamonteiro.rocha@gmail.com>
2026-05-20 10:49:43 +0100

    automação Page Objects

--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -1,13 +1,14 @@
 /// <reference types="cypress"/>
 import { faker } from '@faker-js/faker';
+import cadastroPage from '../support/pages/cadastro.page';
 
 describe('Funcionalidade: Cadastro no Hub de Leitura', () => {
     
     beforeEach(() => {
-        cy.visit('register.html')
+        cadastroPage.visitarPaginaCadastro()
     });
 
-    it('Deve fazer o cadastro com sucesso, usando função JS', () => {
+    it('Deve fazer o cadastro com sucesso - usando função JS', () => {
         let email = `teste${Date.now()}@email.com`
         cy.get('#name').type('Leticia Monteiro')
         cy.get('#email').type(email)
@@ -20,7 +21,7 @@ describe('Funcionalidade: Cadastro no Hub de Leitura', () => {
         cy.url().should('include', 'dashboard')
     });
 
-    it('Deve fazer o cadastro com sucesso, usando o comando', () => {
+    it('Deve fazer o cadastro com sucesso - usando o comando', () => {
         let email = `teste${Date.now()}@email.com`
         cy.preencherCadastro(
             'Leticia Monteiro',
@@ -32,7 +33,7 @@ describe('Funcionalidade: Cadastro no Hub de Leitura', () => {
         cy.url().should('include', 'dashboard')
     });
 
-    it('Deve fazer o cadastro com sucesso, usando Faker', () => {
+    it('Deve fazer o cadastro com sucesso - usando Faker', () => {
         let name = faker.person.fullName()
         let email = faker.internet.email() 
         cy.get('#name').type(name)
@@ -45,6 +46,17 @@ describe('Funcionalidade: Cadastro no Hub de Leitura', () => {
         //Resultado esperado
         cy.url().should('include', 'dashboard')
         cy.get('#user-name').should('contain', name)
+    });
+
+    it('Deve fazer cadastro com sucesso - usando Page Objects', () => {
+        let email = faker.internet.email()
+        cadastroPage.preencherCadastro('Sofia Pereira', email, '11123456789', 'senha123', 'senha123')
+        cy.url().should('include', 'dashboard')
+    });
+
+    it.only('Deve validar mensagem ao tentar cadastrar sem preencher nome', () => {
+        cadastroPage.preencherCadastro('', 'sofia@teste.com', '11123456789', 'senha123', 'senha123')
+        cy.get(':nth-child(1) > .invalid-feedback').should('contain', 'Nome deve ter pelo menos 2 caracteres')
     });
 
 });
--- cypress/e2e/catalogo-busca.cy.js
+++ cypress/e2e/catalogo-busca.cy.js
@@ -15,4 +15,19 @@ describe('Funcionalidade: Busca no Catálogo', () => {
         cy.get('#search-input').type(catalogo[0].livro)
         cy.get('.card-title').should('contain', catalogo[0].livro)
     });
+
+    it('Deve fazer a busca de um livro usando Fixture', () => {
+        cy.fixture('livros').then((cat) =>{
+            cy.get('#search-input').type(cat[0].livro)
+            cy.get('.card-title').should('contain', cat[0].livro)
+        })
+    });
+
+    it.only('Deve validar todos os livros da lista', () => {
+        cy.fixture('livros').then((cat) =>{
+            cat.forEach(item => {
+                cy.get('#search-input').clear().type(item.livro)
+            })
+        })
+    });
 });
